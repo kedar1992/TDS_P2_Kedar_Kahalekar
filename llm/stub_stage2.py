6a5f17a1ba06370b38d8eb408de01669cb985954
kedar1992 <24ds2000116@ds.study.iitm.ac.in>
2025-08-14 10:47:05 +0530

    Update stub_stage2.py

--- llm/stub_stage2.py
+++ llm/stub_stage2.py
@@ -15,25 +15,21 @@ client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
 def generate_analysis_code(task_text: str,  code1: str) -> str:
     prompt = f"""
-You are a data analyst.
-Stage-1 code (for loading and cleaning data):
-{code1}
-
-User request:
+You are a data analyst. 
+You are given a pandas DataFrame named `df`. 
+Write Python code to perform analytical tasks as per User request:
 {task_text}
 
 Strict Rules to follow:
-The DataFrame from Stage-1 is already available as 'df'. Do NOT reload or fetch data.
-Perform the requested analysis using only df.
+The only use provided dataframe df Do NOT reload or fetch data.
 If any numeric column keep the value in absolute units
-Put the result in a variable called analysis_result (JSON-serializable).
-Output only Python code, no explanations.
 Do NOT drop rows unless explicitly asked.
-Use only the column names provided in the schema dictionary. Do not invent or infer any other column names.:
+Use only the column names provided in the given dataframe Do not invent or infer any other column names.:
    - For numeric columns (int64, float64), clean values by:
      * Removing any non-numeric characters except '.' and digits.
      * Handling currency symbols, commas, spaces, and footnotes.
-   - For object columns that contain mixed numeric and text (e.g., '$2,923,706,026'), extract the full numeric value without truncation.
+   - For object columns that contain mixed numeric and text (e.g., '$2,923,706,026'), just keep the full numeric value and remove characters or symbols
+Put the result in a variable called analysis_result (JSON-serializable).
 Output only Python code, no explanations or comments.
 """
     try:
