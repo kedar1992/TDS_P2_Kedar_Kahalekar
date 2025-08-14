bc8f499ba2a8b5ebdb8f2cf59b1ffa4890c6004b
kedar1992 <24ds2000116@ds.study.iitm.ac.in>
2025-08-14 10:26:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,17 +55,17 @@ async def analyze_task(file: UploadFile = File(...)):
         print("Printing schema")
         print(result)
 
-        # # Step 4: Generate analysis code
-        # analysis_code = generate_analysis_code(original_task, result, generated_code_data)
-        # analysis_code_clean = extract_python_code(analysis_code)
-        # ast.parse(analysis_code_clean)
-        # analysis_result = execute_code2(analysis_code_clean, df)
+        # Step 4: Generate analysis code
+        analysis_code = generate_analysis_code(original_task, none, generated_code_data)
+        analysis_code_clean = extract_python_code(analysis_code)
+        ast.parse(analysis_code_clean)
+        analysis_result = execute_code2(analysis_code_clean, df)
 
 
-        # # Step 5: Return results
-        # print("Analysis Result:", analysis_result)
-        # response = {"Schema is ": result, "Analysis_Result is-": make_json_serializable(analysis_result)}
-        response = {"Schema is ": make_json_serializable(result),"code is -": generated_code_data}
+        # Step 5: Return results
+        print("Analysis Result:", analysis_result)
+        response = {"Analysis_Result is-": make_json_serializable(analysis_result)}
+        # response = {"Schema is ": make_json_serializable(result),"code is -": generated_code_data}
 
         return JSONResponse(content=response)
 
